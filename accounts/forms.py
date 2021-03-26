2addfb5ea026debf0622f0e123922fcfb24fe5e2
Pritam055 <pritam.waiba2055@gmail.com>
2021-03-26 14:26:37 +0545

    second commit, upto 14 number video

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 from .models import Order
 
@@ -6,3 +8,14 @@ class OrderForm(forms.ModelForm):
 	class Meta:
 		model = Order
 		fields = '__all__'
+
+
+# the UserCreationForm only gives Password and confirm password
+# and we do not need to include them to the fields cause they comes automatically to the template
+# and getting the remaining needed fields from the default User model
+class CreateUserForm(UserCreationForm):
+
+	class Meta:
+		model = User
+		fields = ['username','email']
+		label = {'email':'Email'}
